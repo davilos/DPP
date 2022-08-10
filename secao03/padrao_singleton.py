aaf622c9ab4d28ccb921467ce927285d2a117f61
davilos <daviloscostagg@hotmail.com>
2022-08-10 15:56:38 -0300

    Padrão Singleton em Python

---
+++ secao03/padrao_singleton.py
@@ -0,0 +1,17 @@
+from typing_extensions import Self
+
+
+class Singleton(object):
+
+    def __new__(cls: type[Self]) -> Self:
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+            print(f'Criando o objeto {cls.instance}')
+        return cls.instance
+
+
+s1 = Singleton()
+print(f'Instância 1: {id(s1)}')
+
+s2 = Singleton()
+print(f'Instância 2: {id(s2)}')
