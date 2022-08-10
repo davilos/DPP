49458fa71a7fa8b5aa917ef84b85695001d8e8c7
davilos <daviloscostagg@hotmail.com>
2022-08-10 16:03:27 -0300

    Singleton nos módulos do Python

---
+++ secao03/singleton_modulos.py
@@ -0,0 +1,12 @@
+import meu_modulo
+
+
+print(f'O nome é {meu_modulo.NOME}')
+
+meu_modulo.funcao1()
+
+import meu_modulo as mm  # Aqui ele não imprime "O módulo foi importado",
+# isso ocorre devido ao Singleton que existe na linguagem, como já tem uma
+# instância criada, ela não cria outra
+
+mm.funcao2()
